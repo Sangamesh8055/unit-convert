4f9b3f122e35e04f0e16291f84f53ac5c9452b9b
Sangamesh8055 <125205705+Sangamesh8055@users.noreply.github.com>
2024-01-08 13:57:55 +0530

    Update unit_converter.py

--- unit_converter.py
+++ unit_converter.py
@@ -1,8 +1,13 @@
 # unit_converter.py
+import sys
+
 def convert_to_meters(feet):
     return feet * 0.3048
 
-user_input = float(input("Enter length in feet: "))
-result = convert_to_meters(user_input)
-print(f"{user_input} feet is equal to {result} meters.")
-
+if __name__ == "__main__":
+    try:
+        user_input = float(sys.argv[1])
+        result = convert_to_meters(user_input)
+        print(f"{user_input} feet is equal to {result} meters.")
+    except ValueError:
+        print("Invalid input. Please provide a valid number.")
